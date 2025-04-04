a3b44413433f723df18ff4273406344a2b66d559
Tomilov-G <tomilovgleb591@gmail.com>
2025-04-04 16:03:36 +0700

    Внесены правки после ревью

--- src/api/services/GadgetsService.ts
+++ src/api/services/GadgetsService.ts
@@ -1,11 +1,17 @@
 import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit"
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import API_URL from "../../constants/constants";
 
-const fetchCatalogItems = createAsyncThunk('catalog/fetchCatalogItems', async () => {
-    const response = await axios.get(`${API_URL}`)
-    return response.data
-})
+const fetchCatalogItems = createAsyncThunk(
+  "catalog/fetchCatalogItems",
+  async () => {
+    try {
+      const response = await axios.get(`${API_URL}`);
+      return response.data;
+    } catch (error) {
+      console.error("Ошибка при попытке получить данные", error);
+    }
+  }
+);
 
-export default fetchCatalogItems
-
+export default fetchCatalogItems;
